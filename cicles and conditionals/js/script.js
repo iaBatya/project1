60e32b77b35cc3803732f0bfad3c57babbd11e7b
iaBatya <dmitryajax@gmail.com>
2022-05-24 11:48:28 +0300

    circles and conditionals final

--- cicles and conditionals/js/script.js
+++ cicles and conditionals/js/script.js
@@ -1,38 +1,111 @@
-let firstQuestion = prompt('How many films did you watch?', '');
-let secondQuestion = prompt('What is the name of the one recently watched movie?', '');
-let thirdQuestion = prompt('How can you rate this movie?', '');
-
-for (let i = 0; i < 3; i++) {
-    if (firstQuestion === null || firstQuestion.length > 5) {
-        break;
-    } else if (secondQuestion === null || secondQuestion.length > 5) {
-        break;
-    } else if (thirdQuestion === null || thirdQuestion.length > 5) {
-        break;
-    }
-    console.log(i);
-}
+let numberOfFilms = prompt('How many films did you watch?', '');
 
 let personalMovieDB = {
-    count: firstQuestion,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
 };
 
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('What is the name of the one recently watched movie?', ''),
+        b = prompt('How can you rate this movie?', '');
+
+        if (a != null && b !== null && a != '' & b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+}
+
 if (personalMovieDB.count < 10) {
     alert('Not many films was watched');
-} else if ( personalMovieDB.count === 10 || personalMovieDB.count <= 30 ) {
+} else if ( personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
     alert('Youre a good fan');
-} else if ( personalMovieDB.count > 30 ) {
+} else if ( personalMovieDB.count >= 30 ) {
     alert('Youre kinoman');
 } else {
     alert('Error');
 }
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 console.log(personalMovieDB);
+
+//good
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let firstQuestion = prompt('How many films did you watch?', '');
+// let secondQuestion = prompt('What is the name of the one recently watched movie?', '');
+// let thirdQuestion = prompt('How can you rate this movie?', '');
+
+// for (let i = 0; i < 3; i++) {
+//     if (firstQuestion === null || firstQuestion.length > 5) {
+//         break;
+//     } else if (secondQuestion === null || secondQuestion.length > 5) {
+//         break;
+//     } else if (thirdQuestion === null || thirdQuestion.length > 5) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// let personalMovieDB = {
+//     count: firstQuestion,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+// };
+
+// if (personalMovieDB.count < 10) {
+//     alert('Not many films was watched');
+// } else if ( personalMovieDB.count === 10 || personalMovieDB.count <= 30 ) {
+//     alert('Youre a good fan');
+// } else if ( personalMovieDB.count > 30 ) {
+//     alert('Youre kinoman');
+// } else {
+//     alert('Error');
+// }
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+// console.log(personalMovieDB);
 
 
